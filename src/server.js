be9c8bbda6588b20b8dddab1c2d169b07af6119f
Carlos Vinicius <viniciusdev.26@gmail.com>
2025-03-08 15:01:22 -0300

    ➖ delete dotenv dependency

--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 
-const dotenv = require("dotenv");
-dotenv.config();
-
 const knex = require("knex")({
 	client: "mysql2",
 	connection: {
