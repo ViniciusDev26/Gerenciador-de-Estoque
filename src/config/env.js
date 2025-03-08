c6a71607502b88f8ab1027a5393433665d1c41db
Carlos Vinicius <viniciusdev.26@gmail.com>
2025-03-08 16:00:16 -0300

    🔧 configure docker compose to up mysql server

--- src/config/env.js
+++ src/config/env.js
@@ -1,11 +1,11 @@
 const ENV = {
 	DATABASE: {
-		HOST: process.env.DB_HOST ?? "localhost",
-		PORT: process.env.DB_PORT ?? 3306,
-		USER: process.env.DB_USER ?? "root",
-		PASSWORD: process.env.PASSWORD,
+		HOST: process.env.DB_HOST || "localhost",
+		PORT: process.env.DB_PORT || 3306,
+		USER: process.env.DB_USER || "root",
+		PASSWORD: process.env.DB_PASSWORD,
 		NAME: process.env.DB_NAME,
-		DEBUG_MODE: process.env.DB_DEBUG ?? false,
+		DEBUG_MODE: process.env.DB_DEBUG || false,
 	},
 	PORT: process.env.PORT ?? 5051,
 };
